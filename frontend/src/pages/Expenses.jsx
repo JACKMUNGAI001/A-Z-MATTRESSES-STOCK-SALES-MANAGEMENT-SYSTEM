5c7ebcb4b896c5ffd0edfbc65639842036461d2a
Jackson <jacksonmungai001@gmail.com>
2025-11-21 03:12:23 +0300

    replace imports from utils to api

--- frontend/src/pages/Expenses.jsx
+++ frontend/src/pages/Expenses.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import api from "../utils/api";
+import api from "../api/api";
 
 export default function Expenses() {
   const [shop, setShop] = useState("");
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import api from "../utils/api";
+import api from "../api/api";
 
 export default function Profile() {
   const [data, setData] = useState({
